65479fb0045a4227917425225f8f31b654e59e5d
Kolfering <benoithauquier@hotmail.com>
2022-12-25 12:33:31 +0100

    Make teleports/media sounds local for 3d sounds when used for local player

--- Source_Files/GameWorld/player.cpp
+++ Source_Files/GameWorld/player.cpp
@@ -1355,7 +1355,7 @@ static void update_player_teleport(
 				{
 					if (View_DoInterlevelTeleportInEffects()) {
 						start_teleporting_effect(false);
-						play_object_sound(player->object_index, Sound_TeleportIn()); 
+						play_object_sound(player->object_index, Sound_TeleportIn(), true); 
 						if (shapes_file_is_m1()) start_fade(_fade_bright);
 					}
 				}
@@ -1411,7 +1411,7 @@ static void update_player_teleport(
 				{
 					if (player->teleporting_destination >= 0 || View_DoInterlevelTeleportInEffects()) {
 						start_teleporting_effect(false);
-						play_object_sound(player->object_index, Sound_TeleportIn()); 
+						play_object_sound(player->object_index, Sound_TeleportIn(), true); 
 						if (shapes_file_is_m1()) start_fade(_fade_bright);
 					}
 					else {
@@ -1469,7 +1469,7 @@ static void update_player_teleport(
 					{
 						start_teleporting_effect(true);
 					}
-					play_object_sound(player->object_index, Sound_TeleportOut());
+					play_object_sound(player->object_index, Sound_TeleportOut(), player_index == current_player_index);
 				}
 				else /* Level change */
 				{
@@ -1478,7 +1478,7 @@ static void update_player_teleport(
 					/* Everyone plays the teleporting effect out. */
 					if (View_DoInterlevelTeleportOutEffects()) {
 						start_teleporting_effect(true);
-						play_object_sound(current_player->object_index, Sound_TeleportOut());
+						play_object_sound(current_player->object_index, Sound_TeleportOut(), player_index == current_player_index);
 					}
 					
 					/* Every players object plays the sound, and everyones monster responds. */
@@ -1567,7 +1567,7 @@ static void update_player_media(
 		
 		if (sound_type!=NONE)
 		{
-			play_object_sound(monster->object_index, get_media_sound(polygon->media_index, sound_type));
+			play_object_sound(monster->object_index, get_media_sound(polygon->media_index, sound_type), player_index == current_player_index);
 		}
 	}
 
@@ -1586,7 +1586,7 @@ static void update_player_media(
 		
 		if (sound_index!=NONE)
 		{
-			play_object_sound(monster->object_index, sound_index);
+			play_object_sound(monster->object_index, sound_index, player_index == current_player_index);
 		}
 	}
 }
